7ca507611f0c9edc431ca008e833a163f0b37545
Malcolm <mail.malcolm.prince@gmail.com>
2025-04-21 10:16:22 +0200

    a bit more

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>About Will</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="geo-block.js"></script>
+</head>
+<body>
+  <header>
+    <img src="images/banner.jpg" class="banner" alt="Banner">
+  </header>
+  <main>
+    <div class="top-section">
+      <img src="images/left.jpg" class="left-image" alt="Left Image">
+      <div class="side-text">
+        <h2>Hi I am Will. Hallo ich bin Will. Hoi ik ben Will</h2>
+        <p>Hello, I'm Will and I want to tell about myself. I am healthhy and well educated. I am soneone who is trustworthy and reliable.</p>
+      </div>
+    </div>
+    <div class="image-grid">
+      <img src="images/img1.jpg" alt="Image 1">
+      <img src="images/img2.jpg" alt="Image 2">
+      <img src="images/img3.jpg" alt="Image 3">
+      <img src="images/img4.jpg" alt="Image 4">
+    </div>
+  </main>
+</body>
+</html>
